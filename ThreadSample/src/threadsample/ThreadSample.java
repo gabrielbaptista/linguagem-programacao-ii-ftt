767dcaff415d5ea3b33b2200cd8ce0db4cef0d1f
gabriell <gabriel@smit.net.br>
2019-08-29 22:37:37 -0300

    Exemplo de Threads

---
+++ ThreadSample/src/threadsample/ThreadSample.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadsample;
+
+import java.time.Instant;
+
+/**
+ *
+ * @author gabriell
+ */
+public class ThreadSample {
+
+    public static int Interacoes = 0;
+    public static final Object objeto = new Object();
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ThreadSampleClass thread1 = new ThreadSampleClass();
+        thread1.start();
+        
+        ThreadSampleClass2 runnableClass = new ThreadSampleClass2();
+        Thread thread2 = new Thread(runnableClass);
+        thread2.start();
+        
+        ThreadSampleClass3 runnableClass3 = new ThreadSampleClass3();
+        Thread thread3 = new Thread(runnableClass3);
+        thread3.start();
+        System.out.printf("%s - Apenas um texto escrito no main Thread!\n", 
+                Instant.now().toString());
+        try {
+            runnableClass3.setEnabled(false);
+            thread3.join(); 
+            System.out.println("Thread 3 encerrada corretamente");
+	} catch(InterruptedException e) { 
+            System.out.println(e.toString());
+        }
+        System.out.printf("Foram realizadas %d interações\n", Interacoes);
+    }
+}
---
+++ ThreadSample/src/threadsample/ThreadSampleClass.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadsample;
+import java.time.Instant;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author gabriell
+ */
+public class ThreadSampleClass extends Thread {
+    @Override
+    public void run(){
+        try {
+            synchronized (ThreadSample.objeto)
+                {
+                    ThreadSample.Interacoes++;
+                }
+            Thread.sleep(1000);
+            
+            System.out.printf("%s - Execução de Thread com herança!\n", 
+                    Instant.now().toString());
+            
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ThreadSampleClass.class.getName()).log(
+                    Level.SEVERE, null, ex);
+        }
+    }            
+}
